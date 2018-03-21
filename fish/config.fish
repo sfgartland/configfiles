c0603273033a5db5337367a38eb6428585b76c07
Severin Gartland <sfgartland@hotmail.com>
2018-03-21 19:46:30 +0100

    Added fish config

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+# Set our default path
+set PATH $PATH /usr/local/sbin /usr/local/bin /usr/bin /opt/anaconda/bin
+set PATH $PATH /home/sfgartland/.yarn/bin
+
+alias e="emacsclient -c"
+alias testnet="ping 8.8.8.8"
